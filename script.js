9c200a034e5606901276aeef18f1d63c5940decb
Sayada <87868109+sayadaa@users.noreply.github.com>
2024-06-25 04:45:46 -0700

    ifewf

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const currentPage = window.location.href;
+
+    // Find all navigation links
+    const navLinks = document.querySelectorAll('.header-nav a');
+
+    // Loop through each link and check if its data-page attribute matches current URL
+    navLinks.forEach(link => {
+        const page = link.getAttribute('data-page');
+        if (currentPage.includes(page)) {
+            link.classList.add('myMentee.html');
+        }
+    });
+});
